1ee7d5e078c8b8d2d87b266000f0359e786a65c5
Jim Crews <crews.jim@gmail.com>
2021-03-17 09:46:48 +1100

    add PS profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,63 @@
+#################################################
+#
+# check if you $PROFILE file exists -> 
+#   $ Test-Path $PROFILE
+#
+# create $PROFILE is required ->
+#   $ New-Item -path $profile -type file -force
+#
+#################################################
+
+
+Set-Location C:\Users\jimc\source\repos\
+
+
+function Get-Time { return $(get-date | foreach { $_.ToLongTimeString() } ) }
+function prompt
+{
+    # Write the time 
+    write-host "[" -noNewLine
+    write-host $(Get-Time) -foreground yellow -noNewLine
+    write-host "] " -noNewLine
+    # Write the path
+    write-host $($(Get-Location).Path.replace($home,"~").replace("\","/")) -foreground green -noNewLine
+    write-host $(if ($nestedpromptlevel -ge 1) { '>>' }) -noNewLine
+    return "> "
+}
+
+
+# LS.MSH 
+# Colorized LS function replacement 
+# /\/\o\/\/ 2006 
+# http://mow001.blogspot.com 
+function LL
+{
+    param ($dir = ".", $all = $false) 
+
+    $origFg = $host.ui.rawui.foregroundColor 
+    if ( $all ) { $toList = ls -force $dir }
+    else { $toList = ls $dir }
+
+    foreach ($Item in $toList)  
+    { 
+        Switch ($Item.Extension)  
+        { 
+            ".exe" {$host.ui.rawui.foregroundColor = "Yellow"} 
+            ".cmd" {$host.ui.rawui.foregroundColor = "Red"} 
+            ".msh" {$host.ui.rawui.foregroundColor = "Red"} 
+            ".vbs" {$host.ui.rawui.foregroundColor = "Red"} 
+            Default {$host.ui.rawui.foregroundColor = $origFg} 
+        } 
+        if ($item.Mode.StartsWith("d")) {$host.ui.rawui.foregroundColor = "Green"}
+        $item 
+    }  
+    $host.ui.rawui.foregroundColor = $origFg 
+}
+
+function lla
+{
+    param ( $dir=".")
+    ll $dir $true
+}
+
+function la { ls -force }
